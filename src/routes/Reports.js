3a433e5bf9704e32bcf8902c1d3fbf77892173fc
Chibuokem <chibuokem_tolu@hotmail.com>
2019-10-25 09:42:32 +0100

    Remove date restriction for admin

--- src/routes/Reports.js
+++ src/routes/Reports.js
@@ -7,7 +7,7 @@ import ReportController from '../controllers/Reports';
 
 const router = express.Router();
 
-router.post('/add', AuthMiddleware.validateToken, ReportMiddleware.validateData, ReportMiddleware.checkIfSessionExists, ReportMiddleware.updateSession, ReportController.addReport);
+router.post('/add', AuthMiddleware.validateToken, ReportMiddleware.validateData, ReportMiddleware.checkIfSessionExists, ReportMiddleware.checkIfUserCanReport, ReportMiddleware.updateSession, ReportController.addReport);
 router.post('/approve/:id', AuthMiddleware.validateToken, ReportMiddleware.checkIfIdExists, ReportMiddleware.checkIfUserHasAccess, ReportController.approve);
 router.post('/reject/:id', AuthMiddleware.validateToken, ReportMiddleware.checkIfIdExists, ReportMiddleware.checkIfUserHasAccess, ReportController.reject);
 router.post('/request-edit/:id', AuthMiddleware.validateToken, ReportMiddleware.checkIfIdExists, ReportMiddleware.checkIfUserCanRequestEdit, ReportController.requestEdit);
