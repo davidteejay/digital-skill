235c2b5da63ef3c7883bf03e886b8916a170572a
Chibuokem <chibuokem_tolu@hotmail.com>
2019-10-27 15:39:07 +0100

    Sync

--- src/models/Users.js
+++ src/models/Users.js
@@ -81,7 +81,9 @@ module.exports = (sequelize, DataTypes) => {
   Users.associate = function(models) {
     // associations can be defined here
     Users.belongsTo(models.Users, { foreignKey: 'partnerId', as: 'partner' })
+    Users.hasOne(models.Users, { foreignKey: 'partnerId', as: 'partnerData' })
     Users.belongsTo(models.Users, { foreignKey: 'adminId', as: 'admin' })
+    Users.hasOne(models.Users, { foreignKey: 'adminId', as: 'adminData' })
     Users.belongsTo(models.Organizations, { foreignKey: 'organizationId', as: 'organization' })
   };
   return Users;
